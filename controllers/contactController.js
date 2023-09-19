b335ea806ab8857482f7a5504e6a451b422e02dd
Olesia Kissa <okissaw@gmail.com>
2023-09-19 21:36:49 +0200

    Format error handler, add crud to controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,16 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+const {
+  constants: { OK, CREATED, BAD_REQUEST, NOT_FOUND },
+} = require("../constants");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 const getAllContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
+  const contacts = await Contact.find();
+  res.status(OK).json(contacts);
 });
 
 //@desc Create new contact
@@ -13,31 +19,65 @@ const getAllContacts = asyncHandler(async (req, res) => {
 const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
-    res.status(400);
+    res.status(BAD_REQUEST);
     throw new Error("All fields are mandatory");
   }
-  res.status(201).json({ message: "Create new contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(CREATED).json(contact);
 });
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(NOT_FOUND);
+    throw new Error(`Contact with id of ${req.params.id} not found`);
+  }
+
+  res.status(OK).json(contact);
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(NOT_FOUND);
+    throw new Error(`Contact with id of ${req.params.id} not found`);
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(OK).json({
+    message: `Updated contact: ${JSON.stringify(updatedContact, 0, 2)}`,
+  });
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(NOT_FOUND);
+    throw new Error(`Contact with id of ${req.params.id} not found`);
+  }
+  await Contact.deleteOne({ _id: contact.id });
+  res.status(OK).json("Contact deleted");
 });
 
 module.exports = {
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,46 +1,43 @@
 const {
-  VALIDATION_ERROR,
-  UNAUTHORIZED,
-  FORBIDDEN,
-  NOT_FOUND,
-  SERVER_ERROR,
+  constants: { BAD_REQUEST, UNAUTHORIZED, FORBIDDEN, NOT_FOUND, SERVER_ERROR },
 } = require("../constants");
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  const statusCode = res.statusCode ? res.statusCode : SERVER_ERROR;
+
   switch (statusCode) {
-    case VALIDATION_ERROR:
+    case BAD_REQUEST:
       res.json({
         title: "Validation Failed",
         message: err.message,
-        stackTrace: err.stackTrace,
+        stackTrace: err.stack,
       });
       break;
     case NOT_FOUND:
       res.json({
         title: "Not found",
         message: err.message,
-        stackTrace: err.stackTrace,
+        stackTrace: err.stack,
       });
     case UNAUTHORIZED:
       res.json({
         title: "Unauthorized",
         message: err.message,
-        stackTrace: err.stackTrace,
+        stackTrace: err.stack,
       });
       break;
     case FORBIDDEN:
       res.json({
         title: "Forbidden",
         message: err.message,
-        stackTrace: err.stackTrace,
+        stackTrace: err.stack,
       });
       break;
     case SERVER_ERROR:
       res.json({
         title: "Server error",
         message: err.message,
-        stackTrace: err.stackTrace,
+        stackTrace: err.stack,
       });
       break;
     default:
